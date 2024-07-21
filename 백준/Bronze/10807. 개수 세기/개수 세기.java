c5483c5e24058ef911d665c8172dfcf11a80b637
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-07-21 15:57:23 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int v = Integer.parseInt(br.readLine());
+
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+      if(Integer.parseInt(st.nextToken()) == v) {
+        cnt++;
+      }
+    }
+    br.close();
+
+    System.out.println(cnt);
+  }
+}
