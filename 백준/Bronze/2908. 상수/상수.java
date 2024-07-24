0d7f64c77ee5e13706791930ebda48f9293f0a91
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-07-25 03:03:25 +0900

    [Bronze II] Title: 상수, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,11 +1,32 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main { // 방법 1
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+    int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+    br.close();
+
+    System.out.println(a > b ? a : b);
+  }
+}
+
+
+/*
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main { // 방법 2
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -35,7 +56,6 @@ public class Main {
     } else {
       bw.write(String.valueOf(num2));
     }
-
     bw.write("\n");
     br.close();
 
@@ -43,3 +63,4 @@ public class Main {
     bw.close();
   }
 }
+*/
