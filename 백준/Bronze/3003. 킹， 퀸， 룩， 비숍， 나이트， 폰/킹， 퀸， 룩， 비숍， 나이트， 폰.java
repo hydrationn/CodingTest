93ab169ef50374ba7103b21119aa9a7e16c3b687
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-07-26 22:02:00 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 148 ms, Memory: 15896 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int[] arr = {1, 1, 2, 2, 2, 8};
+
+    for (int i = 0; i < 6; i++) {
+      int count = arr[i] - Integer.parseInt(st.nextToken());
+      bw.write(count + " ");
+    }
+    bw.write("\n");
+    br.close();
+
+    bw.flush();
+    bw.close();
+  }
+}
