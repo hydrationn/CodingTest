893478b5b3626a8df79bf9d952a13323b5b54716
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-08-11 20:39:30 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 140 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    Long n = Long.parseLong(br.readLine());
+
+    bw.write(n*(n-1)*(n-2)/6 + "\n");
+    bw.write(3 + "\n");
+    br.close();
+
+    bw.flush();
+    bw.close();
+  }
+}
