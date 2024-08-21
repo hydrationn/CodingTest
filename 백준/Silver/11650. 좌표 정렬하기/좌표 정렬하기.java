79bd42bd4773e5f6226dd2b10dd1628115eda7f0
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-08-21 14:01:39 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 772 ms, Memory: 54200 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+
+    int N = Integer.parseInt(br.readLine());
+    int[][] intArr = new int[N][2];
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+
+      intArr[i][0] = Integer.parseInt(st.nextToken());
+      intArr[i][1] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(intArr, (e1, e2) -> {
+      if (e1[0] == e2[0]) {
+        return e1[1] - e2[1];
+      } else {
+        return e1[0] - e2[0];
+      }
+    });
+
+    for (int i = 0; i < N; i++) {
+      bw.write(intArr[i][0] + " " + intArr[i][1] + "\n");
+    }
+
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
