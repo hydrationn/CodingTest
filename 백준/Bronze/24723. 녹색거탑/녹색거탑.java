fc9f80dc58a5528cdc69dd190df498eba7932d45
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-08-24 20:46:32 +0900

    [Bronze IV] Title: 녹색거탑, Time: 108 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    br.close();
+
+    bw.write((int) Math.pow(2, N) + "\n");
+    bw.flush();
+    bw.close();
+  }
+}
