b0655b10a67c8f24c60ebc6270d5862f5d667730
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-08-30 11:09:05 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 112 ms, Memory: 15848 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    bw.write(factorial(N) +"\n");
+
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+
+  public static long factorial(int N) {
+    if (N == 0) {
+      return 1;
+    } else {
+      return N * factorial(N-1);
+    }
+  }
+}
