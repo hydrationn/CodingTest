cdae9bdab836b3d81f5a3fb17ed0b1fe9b8c1381
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-09-23 17:36:07 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1032 ms, Memory: 123792 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -11,10 +11,11 @@ public class Main {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
-    StringBuilder sb = new StringBuilder();
     int n = Integer.parseInt(st.nextToken());
     int m = Integer.parseInt(st.nextToken());
+      
     int[][] dp = new int[n+1][n+1];
+      
     for (int i = 1; i <= n; i++) {
       st = new StringTokenizer(br.readLine());
       for (int j = 1; j <= n; j++) {
@@ -22,16 +23,22 @@ public class Main {
       }
     }
 
+    StringBuilder sb = new StringBuilder();
+      
     for (int k = 1; k <= m; k++) {
       int sum = 0;
+        
       st = new StringTokenizer(br.readLine());
+        
       int x1 = Integer.parseInt(st.nextToken());
       int y1 = Integer.parseInt(st.nextToken());
       int x2 = Integer.parseInt(st.nextToken());
       int y2 = Integer.parseInt(st.nextToken());
+        
       for (int i = x1; i <= x2; i++) {
         sum = sum + (dp[i][y2] - dp[i][y1-1]);
       }
+        
       sb.append(sum + "\n");
     }
     bw.write(sb.toString());
