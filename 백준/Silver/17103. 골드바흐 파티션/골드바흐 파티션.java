84729a6f74d6237850f93e435d21bc6c6404da29
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-10-14 00:47:50 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 284 ms, Memory: 21096 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
   public static void main(String[] args) throws IOException {
