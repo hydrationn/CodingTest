34aece9e43fe5f92c41b1805edae13323e1c0a4e
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-11-21 19:37:35 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+
+    for (int i = 0; i < N; i++) {
+      for (int j = i; j < N; j++) {
+        bw.write("*");
+      }
+      bw.write("\n");
+    }
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
