0115b8bbdb7092cc428024f2a86a225c6fc7681d
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-12-06 01:27:13 +0900

    [Bronze III] Title: 네 수, Time: 120 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    long AB = Long.parseLong(st.nextToken() + st.nextToken());
+    long CD = Long.parseLong(st.nextToken() + st.nextToken());
+
+    long result = AB + CD;
+    bw.write(result + "\n");
+
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
