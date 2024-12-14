8cb6cb450dffb28b46f19999e42c1089f3b50261
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-12-14 23:11:55 +0900

    [Bronze III] Title: 더하기, Time: 120 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+
+    int T = Integer.parseInt(br.readLine());
+    for (int i = 0; i < T; i++) {
+      int N = Integer.parseInt(br.readLine());
+      st = new StringTokenizer(br.readLine());
+
+      int sum = 0;
+      for (int j = 0; j < N; j++) {
+        sum += Integer.parseInt(st.nextToken());
+      }
+      bw.write(sum + "\n");
+    }
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
