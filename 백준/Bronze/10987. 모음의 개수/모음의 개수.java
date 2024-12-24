e0e1907ef3d2a5932f9e1ede734f094720359da9
hydrationn <146078205+hydrationn@users.noreply.github.com>
2024-12-24 23:45:42 +0900

    [Bronze IV] Title: 모음의 개수, Time: 120 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String input = br.readLine();
+
+    int cnt = 0;
+    for (int i = 0; i < input.length(); i++) {
+      if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'o' || input.charAt(i) == 'u')
+        cnt++;
+    }
+    bw.write(cnt + "\n");
+
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
