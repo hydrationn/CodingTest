dff8bb729c31147cf40ffe845e72619250a26067
hydrationn <146078205+hydrationn@users.noreply.github.com>
2025-12-31 23:12:11 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[41][2];
+        
+        // 초기값
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
