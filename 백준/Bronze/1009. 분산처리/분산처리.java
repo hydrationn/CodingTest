30bbb6c2d1ad47172ca76a8c37a60c158d893205
hydrationn <146078205+hydrationn@users.noreply.github.com>
2026-01-09 16:10:29 +0900

    [Bronze II] Title: 분산처리, Time: 948 ms, Memory: 17032 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+    
+    int T = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < T; i++) {
+      st = new StringTokenizer(br.readLine());
+
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+
+      int result = 1;
+      for (int j = 0; j < b; j++) {
+        result *= a;
+        result %= 10;
+      }
+
+      if (result == 0) {
+        bw.write(10 + "\n");
+      } else {
+        bw.write(result + "\n");
+      }
+    }
+
+    bw.flush();
+    bw.close();
+  }
+}
